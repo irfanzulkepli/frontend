cab8e24f3f9e99637b6233a1f73cdaf168c33efa
chungyee.chai <chungyee.chai@imocha.com.my>
2022-02-21 17:53:07 +0800

    Deleted some testing files, updated pipeline-view component (scrollable column WIP)

--- src/app/lms/deals/pipeline-view/pipeline-view.component.ts
+++ src/app/lms/deals/pipeline-view/pipeline-view.component.ts
@@ -3,9 +3,6 @@ import { DndDropEvent } from 'ngx-drag-drop';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
-import { tasks } from './data';
-import { Task } from './kanabn.model';
-
 @Component({
   selector: 'app-pipeline-view',
   templateUrl: './pipeline-view.component.html',
@@ -13,70 +10,39 @@ import { Task } from './kanabn.model';
 })
 export class PipelineViewComponent implements OnInit {
 
-  upcomingTasks: Task[];
-
   constructor(private modalService: NgbModal) { }
 
-  ngOnInit() {
-    this._fetchData();
-  }
+  ngOnInit() { }
 
-  /**
-   * on dragging task
-   * @param item item dragged
-   * @param list list from item dragged
-   */
-  onDragged(item: any, list: any[]) {
-  console.log('item dragged: ', item);
+  cols = [
+    'Col 1',
+    'Col 2',
+    'Col 3'
+  ]
 
-  const index = list.indexOf(item);
-  list.splice(index, 1);
-}
-
-  /**
-   * On task drop event
-   */
-  onDrop(event: DndDropEvent, filteredList?: any[], targetStatus?: string) {
-    if (filteredList && event.dropEffect === 'move') {
-      let index = event.index;
-
-      if (typeof index === 'undefined') {
-        index = filteredList.length;
-      }
-
-      filteredList.splice(index, 0, event.data);
-    }
-  }
-
-  private _fetchData() {
-    // all tasks
-    // this.inprogressTasks = tasks.filter(t => t.status === 'inprogress');
-    this.upcomingTasks = tasks.filter(t => t.status === 'upcoming');
-    // this.completedTasks = tasks.filter(t => t.status === 'completed');
-  }
-
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX – The Rise of Skywalker',
+  deals = [
+    'Deal 1',
+    'Deal 2',
+    'Deal 3'
   ];
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.deals, event.previousIndex, event.currentIndex);
   }
 
   /**
    * Open modal
    * @param content modal content
    */
-   openModal(content: any) {
+   openProfile(content: any) {
     this.modalService.open(content, { size: 'xl'});
   }
 
+    /**
+   * Open modal
+   * @param content modal content
+   */
+     addCard(content: any) {
+      this.modalService.open(content);
+    }
 }
