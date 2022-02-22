08b80e6ebe2f4245615965600491667758255df8
tzefeng.fong <tzefeng.fong@imocha.com.my>
2022-02-22 12:44:18 +0800

    1. Added mock-data for deals module

---
+++ src/app/lms/data/lost-reasons.ts
@@ -0,0 +1,38 @@
+export const LOSTREASONS = [
+    {
+        "id": 2,
+        "lostReason": "Price Quotation",
+        "createdBy": {
+            "id": 1,
+            "firstName": "NG CHIN",
+            "lastName": "TAH",
+            "fullName": "NG CHIN TAH"
+        },
+        "createdAt": null,
+        "updatedAt": null
+    },
+    {
+        "id": 3,
+        "lostReason": "Proposal Failed",
+        "createdBy": {
+            "id": 1,
+            "firstName": "NG CHIN",
+            "lastName": "TAH",
+            "fullName": "NG CHIN TAH"
+        },
+        "createdAt": null,
+        "updatedAt": null
+    },
+    {
+        "id": 4,
+        "lostReason": "Lack of performance",
+        "createdBy": {
+            "id": 1,
+            "firstName": "NG CHIN",
+            "lastName": "TAH",
+            "fullName": "NG CHIN TAH"
+        },
+        "createdAt": null,
+        "updatedAt": null
+    }
+];
