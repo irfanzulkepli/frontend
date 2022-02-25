bf875809549ebe6470012a5c935cf64d0d4f9329
tzefeng.fong <tzefeng.fong@imocha.com.my>
2022-02-25 12:23:29 +0800

    1. Added mock-data for stages in pipelines

---
+++ src/app/lms/data/stages.data.ts
@@ -0,0 +1,282 @@
+export const STAGES = [
+    {
+        "id": 1,
+        "name": "Lead generation",
+        "probability": 1,
+        "pipelineId": 1,
+        "priority": 0,
+        "createdBy": {
+            "id": 1,
+            "firstName": "NG CHIN",
+            "lastName": "TAH",
+            "fullName": "NG CHIN TAH"
+        },
+        "createdAt": "2022-01-04T06:14:56.000000Z",
+        "updatedAt": "2022-02-13T07:26:26.000000Z",
+        "pipeline": {
+            "id": 1,
+            "name": "Lead Pipeline (Sales)",
+            "totalDealValue": 2963220
+        }
+    },
+    {
+        "id": 2,
+        "name": "Lead nurturing",
+        "probability": 100,
+        "pipelineId": 1,
+        "priority": 1,
+        "createdBy": {
+            "id": 1,
+            "firstName": "NG CHIN",
+            "lastName": "TAH",
+            "fullName": "NG CHIN TAH"
+        },
+        "createdAt": "2022-01-04T06:14:56.000000Z",
+        "updatedAt": "2022-02-13T07:26:26.000000Z",
+        "pipeline": {
+            "id": 1,
+            "name": "Lead Pipeline (Sales)",
+            "totalDealValue": 2963220
+        }
+    },
+    {
+        "id": 3,
+        "name": "Marketing qualified lead",
+        "probability": 100,
+        "pipelineId": 1,
+        "priority": 2,
+        "createdBy": {
+            "id": 1,
+            "firstName": "NG CHIN",
+            "lastName": "TAH",
+            "fullName": "NG CHIN TAH"
+        },
+        "createdAt": "2022-01-04T06:14:56.000000Z",
+        "updatedAt": "2022-02-13T07:26:26.000000Z",
+        "pipeline": {
+            "id": 1,
+            "name": "Lead Pipeline (Sales)",
+            "totalDealValue": 2963220
+        }
+    },
+    {
+        "id": 4,
+        "name": "Sales accepted lead",
+        "probability": 100,
+        "pipelineId": 1,
+        "priority": 3,
+        "createdBy": {
+            "id": 1,
+            "firstName": "NG CHIN",
+            "lastName": "TAH",
+            "fullName": "NG CHIN TAH"
+        },
+        "createdAt": "2022-01-04T06:14:56.000000Z",
+        "updatedAt": "2022-02-13T07:26:26.000000Z",
+        "pipeline": {
+            "id": 1,
+            "name": "Lead Pipeline (Sales)",
+            "totalDealValue": 2963220
+        }
+    },
+    {
+        "id": 5,
+        "name": "Sales qualified lead",
+        "probability": 100,
+        "pipelineId": 1,
+        "priority": 4,
+        "createdBy": {
+            "id": 1,
+            "firstName": "NG CHIN",
+            "lastName": "TAH",
+            "fullName": "NG CHIN TAH"
+        },
+        "createdAt": "2022-01-04T06:14:56.000000Z",
+        "updatedAt": "2022-02-13T07:26:26.000000Z",
+        "pipeline": {
+            "id": 1,
+            "name": "Lead Pipeline (Sales)",
+            "totalDealValue": 2963220
+        }
+    },
+    {
+        "id": 6,
+        "name": "Closed deal",
+        "probability": 100,
+        "pipelineId": 1,
+        "priority": 5,
+        "createdBy": {
+            "id": 1,
+            "firstName": "NG CHIN",
+            "lastName": "TAH",
+            "fullName": "NG CHIN TAH"
+        },
+        "createdAt": "2022-01-04T06:14:56.000000Z",
+        "updatedAt": "2022-02-13T07:26:26.000000Z",
+        "pipeline": {
+            "id": 1,
+            "name": "Lead Pipeline (Sales)",
+            "totalDealValue": 2963220
+        }
+    },
+    {
+        "id": 7,
+        "name": "Post-sale",
+        "probability": 100,
+        "pipelineId": 1,
+        "priority": 6,
+        "createdBy": {
+            "id": 1,
+            "firstName": "NG CHIN",
+            "lastName": "TAH",
+            "fullName": "NG CHIN TAH"
+        },
+        "createdAt": "2022-01-04T06:14:56.000000Z",
+        "updatedAt": "2022-02-13T07:26:26.000000Z",
+        "pipeline": {
+            "id": 1,
+            "name": "Lead Pipeline (Sales)",
+            "totalDealValue": 2963220
+        }
+    },
+    {
+        "id": 22,
+        "name": "Lead generation",
+        "probability": 100,
+        "pipelineId": 4,
+        "priority": 0,
+        "createdBy": {
+            "id": 45,
+            "firstName": "demo",
+            "lastName": "demo",
+            "fullName": "demo demo"
+        },
+        "createdAt": "2022-02-12T06:54:55.000000Z",
+        "updatedAt": "2022-02-12T06:54:55.000000Z",
+        "pipeline": {
+            "id": 4,
+            "name": "Test 1",
+            "totalDealValue": 0
+        }
+    },
+    {
+        "id": 23,
+        "name": "Lead nurturing",
+        "probability": 100,
+        "pipelineId": 4,
+        "priority": 1,
+        "createdBy": {
+            "id": 45,
+            "firstName": "demo",
+            "lastName": "demo",
+            "fullName": "demo demo"
+        },
+        "createdAt": "2022-02-12T06:54:55.000000Z",
+        "updatedAt": "2022-02-12T06:54:55.000000Z",
+        "pipeline": {
+            "id": 4,
+            "name": "Test 1",
+            "totalDealValue": 0
+        }
+    },
+    {
+        "id": 24,
+        "name": "Marketing qualified lead",
+        "probability": 100,
+        "pipelineId": 4,
+        "priority": 2,
+        "createdBy": {
+            "id": 45,
+            "firstName": "demo",
+            "lastName": "demo",
+            "fullName": "demo demo"
+        },
+        "createdAt": "2022-02-12T06:54:55.000000Z",
+        "updatedAt": "2022-02-12T06:54:55.000000Z",
+        "pipeline": {
+            "id": 4,
+            "name": "Test 1",
+            "totalDealValue": 0
+        }
+    },
+    {
+        "id": 25,
+        "name": "Sales accepted lead",
+        "probability": 100,
+        "pipelineId": 4,
+        "priority": 3,
+        "createdBy": {
+            "id": 45,
+            "firstName": "demo",
+            "lastName": "demo",
+            "fullName": "demo demo"
+        },
+        "createdAt": "2022-02-12T06:54:55.000000Z",
+        "updatedAt": "2022-02-12T06:54:55.000000Z",
+        "pipeline": {
+            "id": 4,
+            "name": "Test 1",
+            "totalDealValue": 0
+        }
+    },
+    {
+        "id": 26,
+        "name": "Sales qualified lead",
+        "probability": 100,
+        "pipelineId": 4,
+        "priority": 4,
+        "createdBy": {
+            "id": 45,
+            "firstName": "demo",
+            "lastName": "demo",
+            "fullName": "demo demo"
+        },
+        "createdAt": "2022-02-12T06:54:55.000000Z",
+        "updatedAt": "2022-02-12T06:54:55.000000Z",
+        "pipeline": {
+            "id": 4,
+            "name": "Test 1",
+            "totalDealValue": 0
+        }
+    },
+    {
+        "id": 27,
+        "name": "Closed deal",
+        "probability": 100,
+        "pipelineId": 4,
+        "priority": 5,
+        "createdBy": {
+            "id": 45,
+            "firstName": "demo",
+            "lastName": "demo",
+            "fullName": "demo demo"
+        },
+        "createdAt": "2022-02-12T06:54:55.000000Z",
+        "updatedAt": "2022-02-12T06:54:55.000000Z",
+        "pipeline": {
+            "id": 4,
+            "name": "Test 1",
+            "totalDealValue": 0
+        }
+    },
+    {
+        "id": 28,
+        "name": "Post-sale",
+        "probability": 100,
+        "pipelineId": 4,
+        "priority": 6,
+        "createdBy": {
+            "id": 45,
+            "firstName": "demo",
+            "lastName": "demo",
+            "fullName": "demo demo"
+        },
+        "createdAt": "2022-02-12T06:54:55.000000Z",
+        "updatedAt": "2022-02-12T06:54:55.000000Z",
+        "pipeline": {
+            "id": 4,
+            "name": "Test 1",
+            "totalDealValue": 0
+        }
+    }
+];
